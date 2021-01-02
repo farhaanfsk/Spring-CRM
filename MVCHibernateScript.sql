92f61d4da486ed5eaf2a7f7197b74ca1934a1504
farhaanfsk <farhaanfsk@gmail.com>
2021-01-02 19:10:46 +0530

    Added support to docker Build.yml to build the application and docker-compose.yml to run the application

--- MVCHibernateScript.sql
+++ MVCHibernateScript.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `web_customer_tracker`
+CREATE DATABASE  IF NOT EXISTS `web_customer_tracker`;
 USE `web_customer_tracker`;
 
 
@@ -13,13 +13,8 @@ CREATE TABLE `customer` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE `instructor_detail` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `address` varchar(128) DEFAULT NULL,
-  `DOB` DATETIME DEFAULT NULL,
-  `customer_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-  KEY `FK_DETAIL_idx` (`customer_id`),
-  CONSTRAINT `FK_DETAIL` FOREIGN KEY (`customer_id`) 
-  REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+insert into customer values(1,'fsk1','fsk1','fsk1@gmail.com');
+insert into customer values(2,'fsk2','fsk2','fsk2@gmail.com');
+insert into customer values(3,'fsk3','fsk3','fsk3@gmail.com');
+insert into customer values(4,'fsk4','fsk4','fsk4@gmail.com');
+insert into customer values(5,'fsk5','fsk5','fsk5@gmail.com');
